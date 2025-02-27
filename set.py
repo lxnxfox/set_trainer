8b4a1d94dfd1426306525f4e1542e57f37e9fc17
Your Name <you@example.com>
2025-02-27 12:25:24 +0100

    all 81 cards exist

--- set.py
+++ set.py
@@ -8,10 +8,6 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 window.fill((25, 25, 25))
 
-rect = pygame.Rect(30, 30, 60, 80)
-rect_visible = True
-
-
 class Card:
     values = np.array([])
     card_width = 60
@@ -21,6 +17,9 @@ class Card:
         self.x_position = x_position
         self.y_position = y_position
         self.is_visible = is_visible
+    def __str__(self) -> str:
+        output = f"values: {self.values}\nx_position: {self.x_position}\ny_position: {self.y_position}\nis_visible: {self.is_visible}"
+        return output
     def equals(self, card):
         return np.array_equal(self.values, card)
 
@@ -34,13 +33,26 @@ def compare_if_is_set(card1, card2, card3):
             is_set = False
     return is_set
 
+    
+def mix_cards(): # TODO
+    pass
+
+
 
 all_cards = []  # list of all 81 cards
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            for l in range(3):
+                all_cards.append(Card(np.array([i, j, k, l]), 0, 0, False))
+
 
 # test
-all_cards.append(Card(np.array([0,1,2,0]), 30, 30, True))
-all_cards.append(Card(np.array([0,1,2,0]), 120, 30, True))
-all_cards.append(Card(np.array([1,1,2,0]), 210, 30, True))
+all_cards[0].x_position = 30
+all_cards[0].y_position = 30
+all_cards[0].is_visible = True
+print(all_cards[0])
+#all_cards.append(Card(np.array([1,1,2,0]), 210, 30, True))
 
 print("is set: ",compare_if_is_set(all_cards[0], all_cards[1], all_cards[2]))
 print("is equal: ",all_cards[0].equals(all_cards[1]))
@@ -59,8 +71,9 @@ while running:
             for card in all_cards:
                 rect = pygame.Rect(card.x_position, card.y_position, card.card_width, card.card_height)
                 if card.is_visible and rect.collidepoint(event.pos):
-                    rect.visible = False
-
+                    card.is_visible = False
+                    print(all_cards[0])
+    window.fill((25, 25, 25))
     for card in all_cards:
         if card.is_visible:
             pygame.draw.rect(window, "white", (card.x_position, card.y_position, card.card_width, card.card_height))
@@ -69,7 +82,7 @@ while running:
     # pygame.circle(window, "blue", (x, y))
     #pygame.draw.rect(window, "white", (30, 30, 60, 80))
 
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
 
