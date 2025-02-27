b7e859d868b558e786fc126fbf8e3dd874112b53
Your Name <you@example.com>
2025-02-27 16:41:34 +0100

    check set works

--- set.py
+++ set.py
@@ -1,12 +1,49 @@
 import numpy as np
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
 
-window_width = 800
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-window.fill((25, 25, 25))
+        self.window_width = 800
+        self.window_height = 600
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption("SET")
+        self.window.fill((25, 25, 25))
+
+        self.chosen_cards = []
+
+        self.run()
+
+    def run(self):
+        running = True
+        while running:
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for card in all_cards:
+                        rect = pygame.Rect(card.x_position, card.y_position, card.card_width, card.card_height)
+                        if card.is_visible and rect.collidepoint(event.pos):
+                            self.chosen_cards.append(card)
+                            card.is_visible = False
+                if len(self.chosen_cards) == 3:
+                    compare_if_is_set(self.chosen_cards)
+                    self.chosen_cards = []
+
+            self.window.fill((25, 25, 25))
+            for card in all_cards:
+                if card.is_visible:
+                    pygame.draw.rect(self.window, "white", (card.x_position, card.y_position, card.card_width, card.card_height))
+
+            # pygame.draw.rect(window, "red", (x, y, width, height))
+            # pygame.circle(window, "blue", (x, y))
+            pygame.display.flip()
+
+        pygame.quit()
 
 class Card:
     values = np.array([])
@@ -17,20 +54,23 @@ class Card:
         self.x_position = x_position
         self.y_position = y_position
         self.is_visible = is_visible
+
     def __str__(self) -> str:
         output = f"values: {self.values}\nx_position: {self.x_position}\ny_position: {self.y_position}\nis_visible: {self.is_visible}"
         return output
+
     def equals(self, card):
         return np.array_equal(self.values, card)
 
 
-def compare_if_is_set(card1, card2, card3):
+def compare_if_is_set(chosen_cards):
     is_set = True
-    for i in range(4):
-        values = [card1.values[i], card2.values[i], card3.values[i]]
+    for i in range(4): # compares values of each attribute
+        values = [chosen_cards[0].values[i], chosen_cards[1].values[i], chosen_cards[2].values[i]]
         unique_count = len(set(values))
         if(unique_count == 2):  # exact two values are identical
             is_set = False
+    print("is set: ",is_set)
     return is_set
 
     
@@ -60,41 +100,8 @@ for i in range(3):
 
 # test
 mix_cards()
+game = Game()
 
-
-#all_cards.append(Card(np.array([1,1,2,0]), 210, 30, True))
-
-print("is set: ",compare_if_is_set(all_cards[0], all_cards[1], all_cards[2]))
+#print("is set: ",compare_if_is_set(all_cards[0], all_cards[1], all_cards[2]))
 print("is equal: ",all_cards[0].equals(all_cards[1]))
-
-
-# main
-running = True
-while running:
-    # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            for card in all_cards:
-                rect = pygame.Rect(card.x_position, card.y_position, card.card_width, card.card_height)
-                if card.is_visible and rect.collidepoint(event.pos):
-                    card.is_visible = False
-    window.fill((25, 25, 25))
-    for card in all_cards:
-        if card.is_visible:
-            pygame.draw.rect(window, "white", (card.x_position, card.y_position, card.card_width, card.card_height))
-
-    # pygame.draw.rect(window, "red", (x, y, width, height))
-    # pygame.circle(window, "blue", (x, y))
-    #pygame.draw.rect(window, "white", (30, 30, 60, 80))
-
-    pygame.display.flip()
-
-pygame.quit()
-
-
-
 
