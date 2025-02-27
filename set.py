bbcc11186bbedc94249bbf6a88369bb79a39d8b8
Your Name <you@example.com>
2025-02-27 21:51:43 +0100

    cards have content

--- set.py
+++ set.py
@@ -38,17 +38,19 @@ class Game:
             for card in all_cards:
                 if card.is_visible:
                     pygame.draw.rect(self.window, "white", (card.x_position, card.y_position, card.card_width, card.card_height))
+                    draw_content(card, self.window)
 
             # pygame.draw.rect(window, "red", (x, y, width, height))
-            # pygame.circle(window, "blue", (x, y))
             pygame.display.flip()
 
         pygame.quit()
 
+
+
 class Card:
     values = np.array([])
-    card_width = 80
-    card_height = 100
+    card_width = 100
+    card_height = 150
     def __init__(self, values, x_position, y_position, is_visible):
         self.values = values
         self.x_position = x_position
@@ -83,12 +85,54 @@ def mix_cards(): # generates 3x4 matrix of 12 random different cards
                 index = np.random.randint(0, 81)
                 if not np.any(active_cards == all_cards[index]): # card already in active_cards
                     active_cards[i][j] = all_cards[index]
-                    all_cards[index].x_position = 30 + 100*j
-                    all_cards[index].y_position = 30 + 120*i
+                    all_cards[index].x_position = 30 + 130*j
+                    all_cards[index].y_position = 30 + 180*i
                     all_cards[index].is_visible = True
                     card_exists = False
     return active_cards
 
+
+# draws attributes amount [0], shape [1], color [2], strength of color [3]
+def draw_content(card, window):
+    #color
+    strength_of_color = 255 - 87 * card.values[3]
+    if card.values[2] == 0: # red
+        color = (strength_of_color, 0, 0)
+    elif card.values[2] == 1: # green
+        color = (0, strength_of_color, 0)
+    else: # blue
+        color = (0,0, strength_of_color)
+    # amount and shape
+    if card.values[0] == 0: # 1 element
+        if card.values[1] == 0: # rectangle
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+70, 60, 20))
+        elif card.values[1] == 1: # circle
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+75), 20)
+        else: # ellipse
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+70, 60, 20))
+    elif card.values[0] == 1: # 2 elements
+        if card.values[1] == 0: # rectangle
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+45, 60, 20))
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+95, 60, 20))
+        if card.values[1] == 1: # circle
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+50), 20)
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+100), 20)
+        else: # ellipse
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+45, 60, 20))
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+95, 60, 20))
+    elif card.values[0] == 2: # 3 elements
+        if card.values[1] == 0: # rectangle
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+40, 60, 20))
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+70, 60, 20))
+            pygame.draw.rect(window, color, (card.x_position+25, card.y_position+100, 60, 20))
+        if card.values[1] == 1: # circle
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+40), 16)
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+75), 16)
+            pygame.draw.circle(window, color, (card.x_position+45, card.y_position+110), 16)
+        else: # ellipse
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+40, 60, 20))
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+70, 60, 20))
+            pygame.draw.ellipse(window, color, (card.x_position+25, card.y_position+100, 60, 20))
 
 all_cards = []  # list of all 81 cards
 for i in range(3):
@@ -101,7 +145,4 @@ for i in range(3):
 # test
 mix_cards()
 game = Game()
-
-#print("is set: ",compare_if_is_set(all_cards[0], all_cards[1], all_cards[2]))
-print("is equal: ",all_cards[0].equals(all_cards[1]))
 
